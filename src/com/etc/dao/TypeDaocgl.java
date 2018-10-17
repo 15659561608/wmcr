7f964c36406405ce7a357f8c18a5296aaccb8c0d
Administrator <Administrator@stu1003>
2018-10-17 15:47:08 +0800

    提交type

--- src/com/etc/dao/TypeDaocgl.java
+++ src/com/etc/dao/TypeDaocgl.java
@@ -7,10 +7,12 @@ import com.etc.entity.Typecgl;
 import com.etc.util.PageData;
 
 public interface TypeDaocgl {
-
+	
 	PageData<Typecgl> queryType(int page, int pageSize, String keywords);
 
 	List<Type> getType();
 
 	boolean addType(String title, int pId);
+
+	boolean updateType(int id, int pId);
 }
